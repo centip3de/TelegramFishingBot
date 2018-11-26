c9ea9dbcb42b7b965f74bbe420024ee98b79f11c
Kyle Sammons <goodkingkyle@gmail.com>
2018-11-25 19:01:43 -0800

    Stuff

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import telegram
-from telegram.ext import Updater
+from telegram.ext import Updater, CommandHandler
+
 
 def getSecrets():
     f = open("../secrets.txt")
@@ -9,9 +10,19 @@ def getSecrets():
         else:
             return line
 
+def fish(bot, update):
+    print("Doing stuff")
+    bot.send_message(chat_id=update.message.chat_id, text="no u")
+
 def main():
     secret = getSecrets()
     updater = Updater(token=secret)
+    dispatcher = updater.dispatcher
+    fish_handler = CommandHandler('fish', fish)
+    dispatcher.add_handler(fish_handler)
+    print("Polling")
+    updater.start_polling()
+
 
 if __name__ == "__main__":
     main()
