0f09683ca127953778a01eec70ceb5229f7c2b85
Kyle Sammons <ksammon2@uoregon.edu>
2018-11-25 19:49:31 -0800

    Got it working

--- src/main.py
+++ src/main.py
@@ -1,28 +1,48 @@
 import telegram
+import sys
+import os
 from telegram.ext import Updater, CommandHandler
+from pynput.keyboard import Key, Controller
 
+class Main():
+    keyboard = Controller()
 
-def getSecrets():
-    f = open("../secrets.txt")
-    for line in f.readlines():
-        if line[0] == "#":
-            continue
-        else:
-            return line
+    def __init__(self, key):
+        self.key = key
 
-def fish(bot, update):
-    print("Doing stuff")
-    bot.send_message(chat_id=update.message.chat_id, text="no u")
+    def printUsage(self):
+        print("Usage: python3 main.py <key that's bound to fish>")
+        os.exit()
 
-def main():
-    secret = getSecrets()
-    updater = Updater(token=secret)
-    dispatcher = updater.dispatcher
-    fish_handler = CommandHandler('fish', fish)
-    dispatcher.add_handler(fish_handler)
-    print("Polling")
-    updater.start_polling()
+    def getSecrets(self):
+        f = open("../secrets.txt")
+        for line in f.readlines():
+            if line[0] == "#":
+                continue
+            else:
+                return line
+
+    def main(self):
+        def fish(bot, update):
+            print("Doing stuff")
+            Main.keyboard.press(self.key)
+            Main.keyboard.release(self.key)
+            ot.send_message(chat_id=update.message.chat_id, text="no u")
+
+        secret = self.getSecrets()
+        updater = Updater(token=secret)
+        dispatcher = updater.dispatcher
+        fish_handler = CommandHandler('fish', fish)
+        dispatcher.add_handler(fish_handler)
+        print("Polling")
+        updater.start_polling()
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 2:
+        usage()
+
+    key = sys.argv[1]
+    print("Continuting with key:", key)
+    main = Main(key)
+    main.main()
